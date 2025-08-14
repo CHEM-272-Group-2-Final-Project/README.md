396d901262d557837db55b912cf05e087283d266
yyang412 <yejinyang412@gmail.com>
2025-08-14 13:17:43 +0800

    Update Project2.cpp

--- Project2.cpp
+++ Project2.cpp
@@ -2,9 +2,10 @@
 
 CHEM 272_Project 2
 
-Lennard-Jones Potential Simulation 
-using Metropolis Monte-Carlo Method
-C++ Code
+Particle Movement Simulation
+in Lennard-Jones Potential
+using Metropolis Monte-Carlo Algorithm
+-C++ Code
 
 -Group 2-
 David Houshangi
